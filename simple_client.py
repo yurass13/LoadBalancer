41b7a5185d8f3e9ab8224f9cac2d1da4504a29ee
Yury Stolpov <yurass130@gmail.com>
2022-10-17 15:00:08 +0300

    Update: SimpleClient - bugs fixed. SimpleClient - complite.

--- simple_client.py
+++ simple_client.py
@@ -5,19 +5,26 @@ from socket import AF_INET, SOCK_STREAM
 
 class SimpleClient:
     """Class for creating simplest tcp client and sanding data for server."""
-    def __init__(self, ip = '127.0.0.1', port = '8888'):
+    def __init__(self, ip = '127.0.0.1', port = 8080):
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
         self.tcp_socket.connect((ip, port))
 
-    def get_data(self, send_data):
+    def _set_data(self, send_data):
         send_data = send_data.encode('utf-8')
         self.tcp_socket.send(send_data)
 
-    def get_data(self):
+    def _get_data(self):
         recv_data = self.tcp_socket.recv(1024)
         recv_data = recv_data.decode('utf-8')
         return recv_data
 
+    data_provider = property(_get_data, _set_data)
+
     def __del__(self):
         self.tcp_socket.close()
 
+
+if __name__ == '__main__':
+    client = SimpleClient()
+    client.data_provider = "I'm new client!"
+    print(client.data_provider)
