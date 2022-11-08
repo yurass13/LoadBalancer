7001861424f770ae2c9a2177ab3798ef4b032b1d
Yury Stolpov <yurass130@gmail.com>
2022-11-08 16:12:45 +0300

    simple_server rework complete.

--- simple_server.py
+++ simple_server.py
@@ -8,4 +8,22 @@ class SimpleServer(BaseLocalServer):
     """
     def __init__(self, queue_limit = 4, connections_limit = 2):
         super().__init__(queue_limit, connections_limit)
-        self._clients = None
+        self._clients = {}
+
+    def main_process_loop(self):
+        current_id = None
+        while(True):
+            # Если есть таски выполняем 
+            for _ in self._tasks:
+                self._do_tasks()
+
+            if current_id is None:
+                current_id = self.accept_connection()
+            else:
+                value = self._handle_connection(current_id)
+
+                # Если обработчик вернул 0 клиент помечается на удаление
+                if value == 0:
+                    print("Disconnect user:", current_id)
+                    del self._clients[current_id]
+                    current_id = None
